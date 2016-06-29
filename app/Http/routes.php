977b9a2e58e9acbd567b71b2e218efd5597a2b83
Karl Macadangdang <karljarren0308@yahoo.com>
2016-06-30 01:43:51 +0800

    Modified routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,8 +19,15 @@ Route::get('borrowed_books', array('as' => 'cardinal.getBorrowedBooks', 'uses' =
 Route::get('account_information', array('as' => 'cardinal.getAccountInformation', 'uses' => 'CardinalController@getAccountInformation'));
 Route::get('login', array('as' => 'cardinal.getForgotPassword', 'uses' => 'CardinalController@getForgotPassword'));
 Route::get('dashboard', array('as' => 'dashboard.getIndex', 'uses' => 'DashboardController@getIndex'));
+Route::get('loan_books', array('as' => 'dashboard.getLoanBooks', 'uses' => 'DashboardController@getLoanBooks'));
+Route::get('reserved_books', array('as' => 'dashboard.getReservedBooks', 'uses' => 'DashboardController@getReservedBooks'));
+Route::get('receive_books', array('as' => 'dashboard.getReceiveBooks', 'uses' => 'DashboardController@getReceiveBooks'));
+Route::get('manage_records/{what}', array('as' => 'dashboard.getManageRecords', 'uses' => 'DashboardController@getManageRecords'));
+Route::get('add_record/{what}', array('as' => 'dashboard.getAddRecord', 'uses' => 'DashboardController@getAddRecord'));
+Route::get('edit_record/{what}/{id}', array('as' => 'dashboard.getEditRecord', 'uses' => 'DashboardController@getEditRecord'));
 
 Route::post('opac/reserve', array('as' => 'cardinal.postReserve', 'uses' => 'CardinalController@postReserve'));
 Route::post('opac/cancel_reservation', array('as' => 'cardinal.postCancelReservation', 'uses' => 'CardinalController@postCancelReservation'));
-Route::post('data/{key}', array('as' => 'cardinal.postRequestData', 'uses' => 'CardinalController@postRequestData'));
+Route::post('public_data/{key}', array('as' => 'cardinal.postRequestData', 'uses' => 'CardinalController@postRequestData'));
 Route::post('login', array('as' => 'cardinal.postLogin', 'uses' => 'CardinalController@postLogin'));
+Route::post('private_data/{key}', array('as' => 'cardinal.postRequestData', 'uses' => 'CardinalController@postRequestData'));
