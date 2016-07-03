a7688b2f562f605499588291a3b11bbdd85306c0
Karl Macadangdang <karljarren0308@yahoo.com>
2016-07-04 01:12:45 +0800

    Initial commit

---
+++ public/js/dashboard/manage_records/barcodes.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('#books-table').dataTable({
+        aoColumnDefs: [
+            { bSearchable: false, bSortable: false, aTargets: [2] }
+        ]
+    });
+
+    onDataButtonClick('add-button', function() {
+        setModalContent('Add Barcode', '<form data-form="add-barcode-form"><input type="hidden" name="id" value="' + $(this).data('var-id') + '"><div class="form-group"><label for="">Book Title:</label><input type="text" class="form-control" value="' + $(this).data('var-title') + '" disabled></div><div class="form-group"><label for="numberOfCopies">Number of Copies to be Added:</label><input type="number" min="1" name="numberOfCopies" id="numberOfCopies" class="form-control" value="1"></div><div class="form-group text-right no-margin"><input type="submit" class="btn btn-primary" value="Add Barcode"></div></form>', '');
+        openModal();
+    });
+
+    onDynamicDataFormSubmit('add-barcode-form', function() {
+        var serializedInfo = $(this).serialize();
+
+        setModalLoader();
+
+        $.ajax({
+            url: '/add_record/barcodes',
+            method: 'POST',
+            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            data: serializedInfo,
+            dataType: 'json',
+            success: function(response) {
+                setModalContent('Add Barcode Status', response['message'], '');
+
+                setTimeout(function() {
+                    closeModal();
+
+                    location.reload();
+                }, 2000);
+            }
+        });
+
+        return false;
+    });
+});
