876ef0f3472f70e83bbbb535f80b83316dd85cca
Karl Macadangdang <karljarren0308@yahoo.com>
2016-08-02 12:07:34 +0800

    Initial commit

---
+++ app/TblHolidays.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TblHolidays extends Model
+{
+    protected $table = 'tbl_holidays';
+    public $timestamps = false;
+}
---
+++ app/TblReceives.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TblReceives extends Model
+{
+    protected $table = 'tbl_receives';
+    public $timestamps = false;
+}
